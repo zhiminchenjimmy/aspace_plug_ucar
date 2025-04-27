b515d5b9c4d6120f65bf41723b0c9cad585678bf
zhiminchenjimmy <zhiminchen@ucar.edu>
2025-04-27 10:28:55 -0400

    Initial commit with all project files

---
+++ public/models/classification_ext.rb
@@ -0,0 +1,15 @@
+require Rails.root.join("app", "models", "classification")
+
+class Classification < Record
+  private
+
+  # DEPAUL REQUEST (makes sense, this is quick and dirty)
+  def parse_full_title
+    if AppConfig[:pui_classification_title_only]
+      json["title"].to_s
+    else
+      # original method
+      "#{parse_identifier}#{I18n.t("classification.identifier_separator")} #{json["title"]}"
+    end
+  end
+end
